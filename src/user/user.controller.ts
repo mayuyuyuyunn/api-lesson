bdca545662b855b35c5004b81fed1ec6e69c5527
miyake.mayuko <miyake.mayuko.0219@gmail.com>
2024-03-20 17:02:30 +0900

    Add: User認証追加

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,25 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards, Get, Req, Patch, Body } from '@nestjs/common';
+import { Request } from 'express';
+import { AuthGuard } from '@nestjs/passport';
+import { UserService } from './user.service';
+import { User } from '@prisma/client';
+import { UpdateUserDto } from './dto/update-user.dto';
 
+@UseGuards(AuthGuard('jwt'))
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  getLoginUser(@Req() req: Request): Omit<User, 'hashedPassword'> {
+    return req.user;
+  }
+
+  @Patch()
+  updateUser(
+    @Req() req: Request,
+    @Body() dto: UpdateUserDto,
+  ): Promise<Omit<User, 'hashedPassword'>> {
+    return this.userService.updateUser(req.user.id, dto);
+  }
+}
